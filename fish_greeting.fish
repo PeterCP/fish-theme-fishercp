79b7f63e49132335fde459e57d99c5e94641e0fd
PeterCP <pacp95@gmail.com>
2017-06-13 18:13:03 -0500

    Add greeting.

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,21 +1,6 @@
-# function fish_greeting
-    # Customize fish greeting message
-# end
-
 function fish_greeting
-    echo '                 '(set_color F00)'___
-  ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
-/T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')
-[ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')
- \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')
-  \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)
-   \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
-   '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
-  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
-  '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
-   '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
-          '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
-                      '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
-                      (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
-                       (UU)'(set_color normal)
+    set -l user (set_color cyan)(echo "$USER")(set_color normal)
+    # set -l date (set_color green)(date +%H:%M:%S)(set_color normal)
+    set -l date (set_color green)(date +%c)(set_color normal)
+    printf 'Welcome to Fish Shell, %s! Today is %s.' $user $date
 end
