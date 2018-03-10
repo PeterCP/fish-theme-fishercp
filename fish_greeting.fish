62890cf908352473555165146858b65b800b6b4d
Pedro Castillo <pacp95@gmail.com>
2018-03-10 09:47:33 -0600

    Fix missing newline.

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -2,5 +2,5 @@ function fish_greeting
     set -l user (set_color cyan)(echo "$USER")(set_color normal)
     # set -l date (set_color green)(date +%H:%M:%S)(set_color normal)
     set -l date (set_color green)(date +%c)(set_color normal)
-    printf 'Welcome to Fish Shell, %s! Today is %s.' $user $date
+    printf 'Welcome to Fish Shell, %s! Today is %s.\n' $user $date
 end
